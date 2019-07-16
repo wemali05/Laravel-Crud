2d6355cef46219cd2584e06d408dc9737dcd6fd5
wemali05 <alfredwemali05@gmail.com>
2019-07-16 13:20:58 +0300

    Crud functionality in laravel, create, read, update and delete

---
+++ Crud/resources/views/shares/create.blade.php
@@ -0,0 +1,48 @@
+@extends('layout')
+
+@section('content')
+<style>
+  .uper {
+    margin-top: 40px;
+  }
+</style>
+
+<div class="card uper">
+  <div class="card-header">
+    Add Share
+  </div>
+  <div class="card-body">
+  @if($errors->any())
+    <div class="alert alert-danger">
+      <ul>
+        @foreach($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div><br/>
+  @endif
+  </div>
+
+  <form method="post" action="{{route('shares.store')}}">
+  @csrf
+    <div class="form-group">
+      <label for="name">Share Name:</label>
+      <input type="text" class="form-control" name="share_name">
+    </div>
+
+    <div class="form-group">
+      <label for="price">Share Price :</label>
+      <input type="text" class="form-control" name="share_price">
+    </div>
+
+    <div class="form-group">
+      <label for="quantity">Share Quantity:</label>
+      <input type="text" class="form-control" name="share_qty">
+    </div>
+    <button type="submit" class="btn btn-primary">Add</button>
+   
+  </form>
+
+</div>
+
+@endsection
